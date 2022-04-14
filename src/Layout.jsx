384e720967c48d9718c99fd7315f3e6a28e8ff67
Samuel Bell-Bell <samuelbell-bell@macbook-pro-de-samuel.home>
2022-04-14 17:22:44 +0200

    create abstract simple layout  header content component  and made work react18 new index root

---
+++ src/Layout.jsx
@@ -0,0 +1,15 @@
+const Layout = (ComponentHeader, ComponentContent) => {
+    return () => {
+        return (<div className="website">
+            <div className="header-content">
+
+                {ComponentHeader}
+            </div>
+            <div className="principale-content">
+                {ComponentContent}
+            </div>
+        </div>)
+    }
+}
+
+export default Layout
