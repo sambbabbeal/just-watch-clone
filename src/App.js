080c2495b0866d343be9db378d75bb76be4707c8
Samuel Bell-Bell <samuelbell-bell@macbook-pro-de-samuel.home>
2022-04-28 18:18:21 +0200

    appel axios au  imdb affichage des trucs des prefere lastrelease donne que le dernier film todo : merger integrer dans le layout et integrer donné via card creation d un type movie...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
-
+import { MovieGrid } from "./stories/Moviegrid"
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      listMovie: []
+    };
+
+  }
+  componentDidMount() {
+    let ctx = this;
+    axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=ae7c30248f874b6ff1052082be68d6c7`)
+      .then(res => {
+        const movies = res.data.results;
+        console.log(movies, this.state)
+        //  this.setState(state => {
+
+        //     ({ ...state, listMovie: movies })
+
+        //  }, (state2) => {
+        //     console.log(state2, "ok")
+        //   })
+
+        this.setState({ listMovie: [] }, () => this.setState({ listMovie: movies }))
+
+
+
+        console.log("ok", ctx.listMovie, this.state, this.listMovie)
+
+        //this.setState({ persons });
+      })
+    // this.listMovie = ctx.listMovie
+    console.log(this.listMovie)
+  }
   render() {
+    console.log('in render' + this.state.listMovie)
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          {!!this.state.listMovie ? <MovieGrid listMovie={this.state.listMovie}></MovieGrid> : <p>unfind</p>}
+
+        </div>
+        {console.log("coucou", this.listMovie)}
       </div>
     );
   }
--- src/stories/Moviegrid.jsx
+++ src/stories/Moviegrid.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "./moviegrid.css"
 export function MovieGrid(props) {
+    console.log("in movie grid", props.listMovie)
     return (<div className="moviegrid">
-        {props.listMovie.map(e => {
-            return (<div>{e}</div>)
+        coucou
+        {props.listMovie.map((e) => {
+            return (<div>{e.title}</div>)
         })}
     </div>)
 
