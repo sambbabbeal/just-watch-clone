3e83d2c9b6a07368ad4a7c8945feb0d51ccfc40f
Samuel Bell-Bell <samuelbell-bell@macbook-pro-de-samuel.home>
2022-05-16 16:04:46 +0200

    add key index for blocks

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
-import React, { Component, useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-import logo from './logo.svg';
-import './App.css';
-import { MovieGrid } from "./stories/Moviegrid"
-const  App = () => {
+import logo from './logo.svg'
+import './App.css'
+import { MovieGrid } from './stories/Moviegrid'
+const App = () => {
+  const [listMovie, setListMovie] = useState([])
 
-
-  useEffect(()   => {
-    let ctx = this;
-    const [listMovie, setListMovie] = useState([])
-    axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=ae7c30248f874b6ff1052082be68d6c7`)
-      .then(res => {
-        const movies = res.data.results;
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=ae7c30248f874b6ff1052082be68d6c7`
+      )
+      .then((res) => {
+        const movies = res.data.results
         setListMovie(movies)
       })
-  },[])
+  }, [])
 
-
-
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <div>
-          {!!this.state.listMovie ? <MovieGrid listMovie={  props.listMovie}></MovieGrid> : <p>unfind</p>}
-        </div>
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
       </div>
+      <div>
+        {!!listMovie ? (
+          <MovieGrid>
+            <div>test</div>
+          </MovieGrid>
+        ) : (
+          <p>unfind</p>
+        )}
+      </div>
+    </div>
+  )
+}
 /*import Layout from "./Layout"
 
 const FakeHeader = () => {
@@ -62,4 +69,4 @@ class App extends Component {
   
 }*/
 
-export default App;
+export default App
--- src/stories/Moviegrid.jsx
+++ src/stories/Moviegrid.jsx
@@ -1,17 +1,12 @@
-import React from "react";
-import PropTypes from "prop-types";
-import "./moviegrid.css";
+import React from 'react'
+import './moviegrid.css'
 
 export function MovieGrid(props) {
   return (
     <div className="moviegrid">
       {!props.children
-        ? props.listMovie.map((e) => <div>{e}</div>)
+        ? props.listMovie.map((e, index) => <div key={index}>{e}</div>)
         : props.children}
     </div>
-  );
+  )
 }
-
-MovieGrid.propTypes = {
-  moviesList: PropTypes.arrayOf(PropTypes.node).isRequired,
-};
