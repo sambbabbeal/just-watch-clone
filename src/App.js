3ff88e3770318e37b7c3fc5ffa608410df49c19e
Samuel Bell-Bell <samuelbell-bell@macbook-pro-de-samuel.home>
2022-05-16 11:42:18 +0200

    SBL correct review finded error

--- src/App.js
+++ src/App.js
@@ -13,11 +13,8 @@ const  App = () => {
     axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=ae7c30248f874b6ff1052082be68d6c7`)
       .then(res => {
         const movies = res.data.results;
-
-
         setListMovie(movies)
       })
-
   },[])
 
 
