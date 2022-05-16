8f2c008893d813ec27dd3466dc309bc46634349a
Samuel Bell-Bell <samuelbell-bell@macbook-pro-de-samuel.home>
2022-05-16 11:41:19 +0200

    refacto class app component to function app component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import axios from 'axios';
 
 import logo from './logo.svg';
 import './App.css';
 import { MovieGrid } from "./stories/Moviegrid"
-class App extends Component {
+const  App = () => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      listMovie: []
-    };
 
-  }
-  componentDidMount() {
+  useEffect(()   => {
     let ctx = this;
+    const [listMovie, setListMovie] = useState([])
     axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=ae7c30248f874b6ff1052082be68d6c7`)
       .then(res => {
         const movies = res.data.results;
 
 
-        this.setState({ listMovie: [] }, () => this.setState({ listMovie: movies }))
+        setListMovie(movies)
       })
 
-  }
-  render() {
-    console.log('in render' + this.state.listMovie)
+  },[])
+
+
 
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
-
         </div>
         <div>
-          {!!this.state.listMovie ? <MovieGrid listMovie={this.state.listMovie}></MovieGrid> : <p>unfind</p>}
-
+          {!!this.state.listMovie ? <MovieGrid listMovie={  props.listMovie}></MovieGrid> : <p>unfind</p>}
         </div>
-        {console.log("coucou", this.listMovie)}
       </div>
     );
-  }
+  
 }
 
 export default App;
