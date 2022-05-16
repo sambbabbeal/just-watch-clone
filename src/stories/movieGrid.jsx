78af45db6533a9934a2121595f439445719fa1b4
Samuel Bell-Bell <samuelbell-bell@macbook-pro-de-samuel.home>
2022-05-16 11:59:02 +0200

    not dry explain todo

--- src/stories/movieGrid.jsx
+++ src/stories/movieGrid.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import "./moviegrid.css";
 
-/**
- * Primary UI component for user interaction
- */
-export const MovieGrid = ({ size, ...props }) => {
-  // we can init this movieGrid by children props or by listMovie props...
-  // might change in futur redondance... not DRY (TODO)
+export function MovieGrid(props) {
+  console.log("child " + props.children);
   return (
     <div className="moviegrid">
       {!props.children
         ? props.listMovie.map((e) => <div>{e}</div>)
         : props.children}
     </div>
-  )
+  );
 }
 
 MovieGrid.propTypes = {
-  moviesList: PropTypes.arrayOf(PropTypes.node).isRequired
-}
+  moviesList: PropTypes.arrayOf(PropTypes.node).isRequired,
+};
