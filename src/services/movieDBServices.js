840809b1726ef571d230bb826acec728944ad6d1
Samuel Bell-Bell <samuelbell-bell@macbook-pro-de-samuel.home>
2022-05-17 14:41:10 +0200

    add url var api in  servicess

--- src/services/movieDBServices.js
+++ src/services/movieDBServices.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
+const API_URL = 'https://api.themoviedb.org/3/movie'
 
 const movieDBServices = {
   nowMovies: () => {
     return axios.get(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=ae7c30248f874b6ff1052082be68d6c7`
+      `${API_URL}/now_playing?api_key=ae7c30248f874b6ff1052082be68d6c7`
     )
   }
 }
